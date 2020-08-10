1658a36b8afb73c4317015d702dd0b42aeca43ab
Musfirotus <musfirotus@gmail.com>
2020-08-11 06:14:32 +0700

    upload finished

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+const fileUpload = require('express-fileupload')
 const bodyParser = require('body-parser')
-// const jwt = require('jsonwebtoken')
+const cloudinary = require('cloudinary').v2
 const port = 3000
 
 const routeIndex = require('./src/routes/index')
@@ -12,6 +13,11 @@ const routeJwt = require('./src/routes/jwts')
 const dotenv = require('dotenv')
 
 dotenv.config();
+cloudinary.config({
+    cloud_name: 'musfirotus',
+    api_key: '924452587884594',
+    api_secret: '_4L0jhrObd8-5U_Atlcqqo-suMs'
+});
 
 app.use(
     bodyParser.urlencoded({
@@ -19,12 +25,28 @@ app.use(
     })
 )
 app.use(bodyParser.json())
+app.use(fileUpload({
+    useTempFiles: true
+}))
 
 app.use('/', routeIndex)
 app.use('/author', routeAuthor)
 app.use('/post', routePost)
 app.use('/comment', routeComment)
 app.use('/jwt', routeJwt)
+
+app.post('/upload', (req, res) => {
+    const file = req.files.photo;
+    cloudinary.uploader.upload(file.tempFilePath, (err, result) => {
+        res.send({
+            success: true,
+            result: {
+                "image" : result.public_id,
+                "url" : result.url
+            }
+        })
+    })
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
--- src/routes/authors.js
+++ src/routes/authors.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const verify = require('../middleware/verifyToken')
 
 const AuthorController = require('../controllers/authorController');
-const UploadController = require('../controllers/uploadController');
 
 router
     .get('/', verify, AuthorController.getAuthors)
@@ -11,6 +10,5 @@ router
     .post('/', verify, AuthorController.saveAuthor)
     .delete('/del/:id', verify, AuthorController.deleteAuthor)
     .patch('/:id', verify, AuthorController.updateAuthor)
-    .post('/upload', verify, UploadController.uploadPhoto)
 
 module.exports = router;
