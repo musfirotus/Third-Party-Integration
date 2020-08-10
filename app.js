ed15bc84d0c55acdf5903d2531ea68aa757218d2
Musfirotus <musfirotus@gmail.com>
2020-08-11 06:46:58 +0700

    refactor upload

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const app = express()
-const fileUpload = require('express-fileupload')
 const bodyParser = require('body-parser')
-const cloudinary = require('cloudinary').v2
 const port = 3000
 
 const routeIndex = require('./src/routes/index')
@@ -13,11 +11,6 @@ const routeJwt = require('./src/routes/jwts')
 const dotenv = require('dotenv')
 
 dotenv.config();
-cloudinary.config({
-    cloud_name: 'musfirotus',
-    api_key: '924452587884594',
-    api_secret: '_4L0jhrObd8-5U_Atlcqqo-suMs'
-});
 
 app.use(
     bodyParser.urlencoded({
@@ -25,9 +18,6 @@ app.use(
     })
 )
 app.use(bodyParser.json())
-app.use(fileUpload({
-    useTempFiles: true
-}))
 
 app.use('/', routeIndex)
 app.use('/author', routeAuthor)
@@ -35,18 +25,32 @@ app.use('/post', routePost)
 app.use('/comment', routeComment)
 app.use('/jwt', routeJwt)
 
-app.post('/upload', (req, res) => {
-    const file = req.files.photo;
-    cloudinary.uploader.upload(file.tempFilePath, (err, result) => {
-        res.send({
-            success: true,
-            result: {
-                "image" : result.public_id,
-                "url" : result.url
-            }
+// Start of cloudinary
+    const fileUpload = require('express-fileupload')
+    const cloudinary = require('cloudinary').v2
+    const verify = require('./src/middleware/verifyToken')
+    cloudinary.config({
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.API_KEY,
+        api_secret: process.env.API_SECRET
+    });
+    app.use(fileUpload({
+        useTempFiles: true
+    }))
+
+    app.post('/upload', verify, (req, res) => {
+        const file = req.files.photo;
+        cloudinary.uploader.upload(file.tempFilePath, (err, result) => {
+            res.send({
+                success: true,
+                result: {
+                    "image" : result.public_id,
+                    "url" : result.url
+                }
+            })
         })
     })
-})
+// End of cloudinary
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
